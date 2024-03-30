33d2d0cda8071f57919c8a8be5ffaa16fb8ced1b
ViktorPetrovSoftuni <viktor.petrov.highschool@buditel.bg>
2024-03-30 11:53:08 +0200

    Added some regex patterns for language keywords

--- vlang.c
+++ vlang.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 void parseVLang(const char *line) {
+    regex_t regex;
+    int reti;
+
+    // Define regex patterns
+    // variable assignment regex
+    char *pattern_print = "print (.+)"; // validated, works, group1 is item to be printed
+    char *pattern_assignment = "^(int|string|bool) ([a-zA-Z]+)(\\s= )(.+)"; // validated, works, group1 is type, group 2 is name, group 4 is value
 }
 
 int main(int argc, char *argv[]) {
 
-return 0;
+    return 0;
 }
