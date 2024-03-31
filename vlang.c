73fef11e459946810973cff494917b16a4333607
ViktorPetrovSoftuni <viktor.petrov.highschool@buditel.bg>
2024-03-31 04:14:28 +0300

    Added error handling code to main compiler file

--- vlang.c
+++ vlang.c
@@ -50,21 +50,58 @@ void parseVLang(const char *line) {
       "read (.+)"; // validated, works, group1 is the information to be read
 }
 
-int main(int argc, char *argv[]) { 
-    FILE *oldFile, *newFile;
+int main(int argc, char *argv[]) {
+  FILE *oldFile, *newFile;
 
-    // Check if correct number of command line arguments are provided
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s oldFile newFile\n", argv[0]);
-        return 1;
-    }
+  // Check if correct number of command line arguments are provided
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s oldFile newFile\n", argv[0]);
+    return 1;
+  }
 
-    // Open oldFile in read mode and newFile in write mode
-    oldFile = fopen(argv[1], "r");
-    if (oldFile == NULL) {
-        fprintf(stderr, "Error opening %s for reading\n", argv[1]);
-        return 1;
-    }
+  // Open oldFile in read mode and newFile in write mode
+  oldFile = fopen(argv[1], "r");
+  if (oldFile == NULL) {
+    fprintf(stderr, "Error opening %s for reading\n", argv[1]);
+    return 1;
+  }
+  // Concatenate ".c" extension to the new file name
+  char newFileName[100];
+  sprintf(newFileName, "%s.c", argv[2]);
 
-    return 0; 
+  // Open newFile with ".c" extension for writing
+  newFile = fopen(newFileName, "w");
+  if (newFile == NULL) {
+    fprintf(stderr, "Error opening %s for writing\n", newFileName);
+    fclose(oldFile);
+    return 1;
+  }
+
+  char line[1000];
+  // Read each line from the input file and call parseVLang function
+  while (fgets(line, sizeof(line), oldFile)) {
+    parseVLang(line);
+  }
+
+  // Close files
+  fclose(newFile);
+
+  // Close the input file
+  fclose(oldFile);
+  if (errorCounter == 1) {
+    printf("\nYour code has an error! Fix it!\n");
+    printf("Error is on line %d\n", errorLines[0]);
+  } else if (errorCounter > 1) {
+    printf("\nYou have %d errors! Fix them!\n", errorCounter);
+    for (int i = 0; i < 512; i++) {
+      if (errorLines[i] == 0) {
+        continue;
+      }
+      printf("Error is on line %d\n", errorLines[i]);
     }
+  } else {
+    printf("\nParsing successful\n");
+  }
+
+  return 0;
+}
